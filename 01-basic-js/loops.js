8372c36072866452bf787954db336738c69bdfdb
Soumya Ranjan Nayak <soumyaranjann7894@gmail.com>
2024-06-26 06:24:22 +0000

    done

---
+++ 01-basic-js/loops.js
@@ -0,0 +1,65 @@
+//web bocket - 5 times
+// console.log("web bocket")
+// console.log("web bocket")
+// console.log("web bocket")
+// console.log("web bocket")
+// console.log("web bocket")
+
+// in loop
+
+// for(let i=1; i <=5; i++)
+//     {
+//         console.log("web bocket")
+//     }
+
+ 
+// let sum=0;
+// for(let i=1; i <=5; i++)
+//         {
+//             sum= sum+i;
+//             console.log(sum)
+//         }
+
+
+// let i =1;
+// while(i <=5)
+//     {
+//         console.log("i = ", i)
+//         i++;
+//     }
+
+// let i = 1;
+// do{
+//     console.log("web bocket");
+//     i++;
+// }
+// while(i<= 10)
+
+// let str="webbocket";
+// //iterate the characters
+// for(let i of str)
+//     {
+//         console.log(i)
+//     }
+
+// let str= "javascript";
+// let size = 0;
+// for(let i of str)
+//     {
+//         console.log("i=",i);
+//         size++;
+//     }
+//     console.log("string size =", size)
+
+let student={
+    name:"soumya",
+    age:22,
+    CGPA:9.7,
+    isPass:true,
+}
+
+for(let key in student)
+    {
+        console.log("key = ",key,"value = ", student[key])
+    }
+
